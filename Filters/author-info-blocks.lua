7b993f5ff34e1d871bd7bed97243fff1d63cd0ab
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2022-04-19 15:39:45 +1000

    updated filters - old ones no longer work.

--- Filters/author-info-blocks.lua
+++ Filters/author-info-blocks.lua
@@ -1,12 +1,9 @@
 --[[
 affiliation-blocks – generate title components
-
-Copyright © 2017–2020 Albert Krewinkel
-
+Copyright © 2017–2021 Albert Krewinkel
 Permission to use, copy, modify, and/or distribute this software for any purpose
 with or without fee is hereby granted, provided that the above copyright notice
 and this permission notice appear in all copies.
-
 THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
 REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
 FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
